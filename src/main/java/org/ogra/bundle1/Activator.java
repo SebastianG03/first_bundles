4a57af67ec2171783eecce5928fc1e98cd6b84b9
Christian Jácome <129555977+ChrisJMora@users.noreply.github.com>
2024-05-13 14:07:26 -0500

    First Commit

---
+++ src/main/java/org/ogra/bundle1/Activator.java
@@ -0,0 +1,42 @@
+package org.ogra.bundle1;/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+public class Activator implements BundleActivator, PallindromeService {
+    private ServiceRegistration serviceReg;
+
+    public void start(BundleContext context) {
+        System.out.println("Starting the bundle1");
+        serviceReg = context.registerService(PallindromeService.class.getName(), this, null);
+    }
+
+    public void stop(BundleContext context) {
+        System.out.println("Stopping the bundle1");
+    }
+
+    public boolean isPallindrome(String a) {
+        int n = a.length();
+        String b = "";
+        for (int i = n - 1; i >= 0; i--) {
+            b = b + a.charAt(i);
+        }
+        return b.equalsIgnoreCase(a);
+    }
+}
---
+++ src/main/java/org/ogra/bundle1/PallindromeService.java
@@ -0,0 +1,5 @@
+package org.ogra.bundle1;
+
+public interface PallindromeService {
+    public boolean isPallindrome(String str) ;
+}
